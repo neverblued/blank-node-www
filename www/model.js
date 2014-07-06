5bf17baba533a9e46905a4497c8dbfff994ea8a4
Dmytro Pinskyi <neverblued@gmail.com>
2014-07-06 08:47:44 +0300

    update

--- www/model.js
+++ www/model.js
@@ -5,5 +5,18 @@ model.now = function(){
 };
 
 model.site = {
-	title: 'Project Title'
+	title: 'Project Title',
+	domain: 'example.com'
 };
+
+model.google = {
+	analytics: '_GOOGLE_ANALYTICS_ID_'
+};
+
+model.facebook = {
+	id: '_FACEBOOK_ID_',
+	link: function(){
+		return 'https://www.facebook.com/' + model.facebook.id;
+	}
+};
+
